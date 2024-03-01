c6c59ceade2df106d3ed5c9070b884b29817b359
Pavithra Chamod J <84024015+Pavithra-cj@users.noreply.github.com>
2024-03-02 02:16:01 +0530

    Splash screen done. Basic requirements of the assesment done.

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SplashScreen from './src/screens/SplashScreen';
 import HomeScreen from './src/screens/HomeScreen';
-import Navigation from './src/routes/navigation';
 
-export default function App() {
+const Stack = createStackNavigator();
 
+const App = () => {
   return (
-    <View style={styles.container}>
-      <HomeScreen/>
-      {/* <Navigation /> */}
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash" headerMode="none">
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ccc',
-  },
-});
+export default App;
---
+++ src/screens/SplashScreen/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3A86A8',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  text: {
+    color: '#FFF',
+    fontSize: 20,
+    marginTop: 20,
+    fontWeight: 'bold'
+  },
+});
+
+export default styles;
